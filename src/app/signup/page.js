988c0e9c00070aed8b6483c5a5b2c058c1665a8f
Luisa Maria Escobar <luisa.escobar@hoist.tech>
2023-05-15 12:04:59 -0500

    SIGN UP, LOGIN AND ACTIVATED ACC

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -5,15 +5,16 @@ import pattern1 from '../../../public/pattern_1.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import CustomButton from '../components/CustomButton';
-import Link from 'next/link';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const page = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
 
   return (
     <div className='w-full flex flex-col items-center justify-center h-fit min-h-screen pt-20'>
-      <div className='w-full md:h-full h-fit max-w-7xl p-9 flex md:flex-row flex-col gap-9 md:justify-between'>
+      <div className='w-full md:h-full h-fit max-w-7xl p-9 flex md:flex-row flex-col gap-9 md:justify-between items-center'>
         <div className='w-[226px] h-[339px] md:block hidden relative'>
           <Image
             alt='patron1'
@@ -27,25 +28,28 @@ const page = () => {
         </div>
         <div className='md:w-1/2 w-full items-center flex flex-col h-max'>
           <div className='w-fit'>
-            <h1 className='text-2xl font-semibold mt-2 px-2'>Inicio de sesión</h1>
+            <h1 className='text-2xl font-semibold mt-2 px-2'>Crea tu cuenta</h1>
             <div className='w-full h-1 bg-chinese-blue rounded-full'></div>
           </div>
           <div className='flex h-fit w-[90%] max-w-[300px] flex-col mt-2'>
             <label htmlFor='search' className='mt-4 text-lg font-semibold'>
-              Usuario:
+              Correo:
             </label>
             <div className='flex h-fit items-center mt-1'>
               <input
-                id='search'
                 type='text'
-                placeholder='Ingresa tu usuario...'
+                placeholder='Ingresa tu correo UN'
                 className='pl-4 pr-10 py-2 w-full rounded-lg border-[3px] border-eerie-black/40 text-base focus:border-[3px] focus:border-chinese-blue focus:outline-none'
               />
             </div>
-            <span className='text-sm font-bold mt-1'>
-              <span className='text-red-700'>Nota:</span> Tu usuario es todo lo que está antes de
-              @uninorte.edu.co en tu correo institucional.
-            </span>
+            <ul className='text-sm font-semibold list-disc px-6 mt-2'>
+              <li>Debe ser @uninorte.edu.co</li>
+              <li>Tu usuario será todo lo que esté antes de @uninorte.edu.co</li>
+              <li>
+                Asegúrate de tener acceso al correo para activar tu cuenta{' '}
+                <span className='text-chinese-blue'>QualiUN</span>
+              </li>
+            </ul>
           </div>
           <div className='flex h-fit w-[90%] max-w-[300px] flex-col'>
             <label htmlFor='search' className='mt-4 text-lg font-semibold'>
@@ -54,10 +58,9 @@ const page = () => {
             <div className='flex h-fit items-center mt-1'>
               <input
                 type={showPassword ? 'text' : 'password'}
-                placeholder='Ingresa tu contraseña...'
+                placeholder='Ingresa tu contraseña'
                 className='pl-4 pr-10 py-2 w-full rounded-lg border-[3px] border-eerie-black/40 text-base focus:border-[3px] focus:border-chinese-blue focus:outline-none'
               />
-
               <button
                 type='submit'
                 className='-ml-[36px] text-chinese-blue hover:text-dust-storm'
@@ -70,18 +73,22 @@ const page = () => {
                 )}
               </button>
             </div>
+            <ul className='text-sm font-semibold list-disc px-6 mt-2'>
+              <li>NO tiene que ser la misma contraseña que la de los servicios UN</li>
+              <li>Mínimo 8 caracteres</li>
+              <li>Mínimo 1 número y 1 letra</li>
+            </ul>
           </div>
           <CustomButton
             width={'w-full max-w-[200px] min-w-fit mt-8'}
             type={1}
             textS={2}
-            text={'Iniciar sesión'}
+            text={'Crear cuenta'}
             action={() => {
-              alert('Hola');
+              router.push('/account-activation');
             }}
             icon='none'
           ></CustomButton>
-          {/** Insertar variable que contenga error */}
           {false && (
             <span
               id='error-msg'
@@ -89,13 +96,10 @@ const page = () => {
             >
               {/** Insertar mensajes de error */}
               {
-                'Si no puedes iniciar sesión, por favor comunícate con el administrador del sistema.'
+                'Asegúrate de que tu correo sea @uninorte.edu.co y que tu contraseña cumpla con los requisitos'
               }
             </span>
           )}
-          <Link href='/signup' className='text-sm mt-2 font-bold w-full max-w-[300px] text-center'>
-            ¿No tienes cuenta de QualiUN?
-          </Link>
         </div>
         <div className='w-[226px] h-[339px] md:block hidden relative'>
           <Image
